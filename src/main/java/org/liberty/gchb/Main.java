634dd8f2c2e64ca004e37f8347d41aa97a20239f
swanandm <swanand01.mehta@gmail.com>
2025-02-09 14:49:42 +0100

    move commit creation methods to different class

--- src/main/java/org/liberty/gchb/Main.java
+++ src/main/java/org/liberty/gchb/Main.java
@@ -3,6 +3,7 @@ package org.liberty.gchb;
 import org.liberty.gchb.application.port.CommitClient;
 import org.liberty.gchb.application.port.ConfigClient;
 import org.liberty.gchb.application.port.FileSystemClient;
+import org.liberty.gchb.domain.service.CommitService;
 import org.liberty.gchb.domain.service.ConfigService;
 import org.liberty.gchb.domain.service.HistoryService;
 import org.liberty.gchb.infrastructure.adapter.cli.GitCommitClient;
@@ -17,8 +18,10 @@ public class Main {
     CommitClient commitClient = new GitCommitClient();
 
     ConfigService configService = new ConfigService(configClient);
+
+    CommitService commitService = new CommitService(configService);
     HistoryService historyService =
-        new HistoryService(configService, fileSystemClient, commitClient);
+        new HistoryService(configService, fileSystemClient, commitClient, commitService);
 
     historyService.createCommitHistory();
   }
---
+++ src/main/java/org/liberty/gchb/domain/service/CommitService.java
@@ -0,0 +1,81 @@
+package org.liberty.gchb.domain.service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import org.liberty.gchb.domain.model.Commit;
+import org.liberty.gchb.domain.model.Config;
+
+public class CommitService {
+  protected final Config config;
+  private final Random random;
+
+  public CommitService(ConfigService configService) {
+    this.config = configService.getConfig();
+    this.random = new Random();
+  }
+
+  public List<Commit> getCommitList() {
+    long totalDaysToProcess = calculateTotalDays();
+    long totalAvailableMinutes = calculateAvailableMinutes();
+
+    return LongStream.range(0L, totalDaysToProcess)
+        .mapToObj(
+            i -> {
+              LocalDate date = config.getStartDate().plusDays(i);
+              return getCommitListByDay(date, totalAvailableMinutes);
+            })
+        .flatMap(List::stream)
+        .sorted(Comparator.comparing(Commit::time))
+        .toList();
+  }
+
+  private int calculateCommitsForDay() {
+    int minCommit = config.getMinCommitPerDay();
+    int maxCommit = config.getMaxCommitPerDay();
+    return random.nextInt(minCommit, maxCommit + 1);
+  }
+
+  private long calculateAvailableMinutes() {
+    LocalTime startTime = config.getStartTime();
+    LocalTime endTime = config.getEndTime();
+    return ChronoUnit.MINUTES.between(startTime, endTime);
+  }
+
+  private long calculateTotalDays() {
+    LocalDate startDate = config.getStartDate();
+    LocalDate endDate = config.getEndDate();
+    if (startDate.isEqual(endDate)) {
+      return 1;
+    }
+    return ChronoUnit.DAYS.between(startDate, endDate);
+  }
+
+  private List<Commit> getCommitListByDay(LocalDate date, long diffInMin) {
+    int totalNumberOfCommit = calculateCommitsForDay();
+    return IntStream.range(0, totalNumberOfCommit)
+        .mapToObj(
+            e -> {
+              LocalDateTime commitDateTime = getCommitDateTime(date, diffInMin);
+              return new Commit(commitDateTime);
+            })
+        .toList();
+  }
+
+  private LocalDateTime getCommitDateTime(LocalDate date, long diffInMin) {
+    long randomMin = random.nextLong(diffInMin);
+    int randomSec = random.nextInt(60);
+    int randomNanoSec = random.nextInt(999999999);
+
+    LocalTime commitTime =
+        config.getStartTime().plusMinutes(randomMin).withSecond(randomSec).withNano(randomNanoSec);
+
+    return date.atTime(commitTime);
+  }
+}
